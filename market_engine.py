04d228350c45868c4c6eeacf561a91128ac3090b
marikanmarikan320-stack <marikanmarikan320@gmail.com>
2026-03-12 15:12:48 +0100

    Update market_engine.py

--- market_engine.py
+++ market_engine.py
@@ -1,1 +1,44 @@
 
+import pandas as pd
+import sqlite3
+import requests
+import datetime
+
+class MarketEngine:
+
+    def __init__(self, api, account):
+
+        self.api = api
+        self.account = account
+        self.db = sqlite3.connect("market.db")
+
+    async def initialize_database(self):
+
+        cursor = self.db.cursor()
+
+        cursor.execute("""
+
+        CREATE TABLE IF NOT EXISTS candles(
+
+        time TEXT,
+        open REAL,
+        high REAL,
+        low REAL,
+        close REAL,
+        volume REAL
+
+        )
+
+        """)
+
+        self.db.commit()
+
+    async def download_initial_candles(self, symbol):
+
+        print("Downloading history...")
+
+    async def update_live_candles(self, symbol):
+
+        print("Updating candles...")
+
+        return pd.DataFrame()
