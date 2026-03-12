1e64635f1ca663c9afc262bb178feff8754cef7a
marikanmarikan320-stack <marikanmarikan320@gmail.com>
2026-03-12 15:18:44 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,19 @@
 
+from telegram import Bot
+
+class TelegramEngine:
+
+    def __init__(self, token, chat):
+
+        self.bot = Bot(token)
+        self.chat = chat
+
+    async def send(self, text):
+
+        await self.bot.send_message(
+
+            chat_id=self.chat,
+
+            text=text
+
+        )
